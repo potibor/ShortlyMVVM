302d955041315c04f400ea5a8aad76ab8d6059ac
Hasan Ozan AL <>
2021-07-09 22:42:40 +0300

    [ozanal] Home ui is created

--- app/src/main/java/com/example/shortlyappipeuya/scenes/HomeFragment.kt
+++ app/src/main/java/com/example/shortlyappipeuya/scenes/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources.getDrawable
 import androidx.fragment.app.viewModels
 import com.example.shortlyappipeuya.R
 import com.example.shortlyappipeuya.databinding.FragmentHomeBinding
--- app/src/main/java/com/example/shortlyappipeuya/scenes/HomeViewModel.kt
+++ app/src/main/java/com/example/shortlyappipeuya/scenes/HomeViewModel.kt
@@ -1,9 +1,22 @@
 package com.example.shortlyappipeuya.scenes
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor() : ViewModel() {
+
+    val shortenLinkText = MutableLiveData<String>()
+    val isShortenLinkTextEmpty = MutableLiveData<Boolean>()
+
+    fun onShortenButtonClicked() {
+        if (shortenLinkText.value?.isNotEmpty() == true) sendLinkToBeShortened()
+        else isShortenLinkTextEmpty.value = true
+    }
+
+    private fun sendLinkToBeShortened() {
+
+    }
 }
